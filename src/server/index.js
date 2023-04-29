300164888d6000e584cb31e14b4d256f3c699e29
Lucas Lima <developerlucaslima@gmail.com>
2023-04-29 17:24:37 -0300

    header

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,25 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((req, res) => {
-  return res.end('hello world')
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+    return res
+    .setHeader('Content-Type', 'application/json')
+    .end(JSON.stringify(users))
+  }
+
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'john.doe@gmail.com'
+    })
+
+    return res.end('Successfully')
+  }
 })
 
 server.listen(3333)
