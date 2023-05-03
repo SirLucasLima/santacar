fc9eb2a8b889ac66a3201ec1ae5f039a444f5c0b
JoviTech <developerlucaslima@gmail.com>
2023-05-03 03:15:21 -0300

    regex to found route parameter

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,14 @@
 import { randomUUID } from 'node:crypto'
 import { Database } from '../database/database.js'
+import { buildRoutePath } from '../utils/build-route-path.js'
 
 const database = new Database()
 
 export const routes = [
-  {
-    method: 'GET',
-    path: '/users',
-    handler: (req, res) => {
-      const users = database.select('users')
-      return res.end(JSON.stringify(users))
-    }
-  },
-  {
+
+  { //create user
     method: 'POST',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
       const { name, email } = req.body
       const user = {
@@ -25,6 +19,21 @@ export const routes = [
 
       database.insert('users', user)
       return res.writeHead(201).end()
+    }
+  },
+  { //list all users
+    method: 'GET',
+    path: buildRoutePath('/users'),
+    handler: (req, res) => {
+      const users = database.select('users')
+      return res.end(JSON.stringify(users))
+    }
+  },
+  { //delete user
+    method: 'DELETE',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      
     }
   }
 ]
